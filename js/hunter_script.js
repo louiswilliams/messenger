0a6da0abaed5ab1b091e0352ad314bfbd8ef5bc4
hrosenblume <hrosenblume@gmail.com>
2015-05-02 16:10:43 -0400

    quick push for chris

---
+++ js/hunter_script.js
@@ -0,0 +1,9 @@
+console.log("boobies");
+
+$( "._4bl8._4bl7" ).click(function() {
+	alert( "We nabbed the new message click." );
+	// $("._2ei6._14-8._5vn4").bind("DOMSubtreeModified", function() {
+	//     alert("tree changed");
+	// });
+	//_2ei6 _14-8 _5vn4
+});
